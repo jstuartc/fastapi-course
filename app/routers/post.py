d8a32cd1cec4433dcbe8b001aad2fe0b98b08e81
jstuartc <james.chapman@me.com>
2022-05-13 10:04:10 +0100

    moved workflows folder

--- app/routers/post.py
+++ app/routers/post.py
@@ -15,7 +15,7 @@ router = APIRouter(
 
 
 
-@router.get("/", response_model=List[schemas.PostOut])  # List stuff is cool
+@router.get("/", response_model=List[schemas.PostOut])  # List stuff is cool - not sure why it is a list...
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
             limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute('''SELECT * FROM posts''')
